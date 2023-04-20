fb5bebed0a340f76d8bbbad78773035942fccc4c
Kiritozhenggang <127064719+Kiritozhenggang@users.noreply.github.com>
2023-04-20 16:20:17 +0800

    Update CSS Slideshow.js

--- 441/CSS Slideshow.js
+++ 441/CSS Slideshow.js
@@ -1,18 +1,22 @@
+ var slideIndex = 1;
+showSlides(slideIndex);
 
-var slideIndex = 1;
-showDivs(slideIndex);
+function plusSlides(n) 
+ {
+ showSlides(slideIndex += n);
+ }
 
-function plusSlides(n) {
-  showDivs(slideIndex += n);
+ function showSlides(n) {
+ var i;
+ var slides = document.getElementsByClassName("mySlides");
+if (n > slides.length) {
+slideIndex = 1
 }
-
-function showSlides(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+if (n < 1) {
+slideIndex = slides.length
 }
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+slides[slideIndex - 1].style.display = "block";
+ }
